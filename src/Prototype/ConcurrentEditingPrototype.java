bbaa6d2f77407176f4c038250a7d3a3b1546b64c
rubilia <rubilia@DESKTOP-QVCNEN2>
2023-10-25 03:47:33 -0700

    Finalized UI

--- src/Prototype/ConcurrentEditingPrototype.java
+++ src/Prototype/ConcurrentEditingPrototype.java
@@ -19,31 +19,35 @@ public class ConcurrentEditingPrototype {
     private Stage stage;
     private Stage primaryStage;
     
+    // Separating UI elements into class variables
+    private ComboBox<String> projectSelect, logEntrySelect;
+    private Button clearLogButton, updateButton, deleteButton, splitButton, proceedButton;
+    private Label selectProjectLabel, clearLogLabel, effortLogEntryLabel, modifyLabel, detailsLabel, updateLabel, deleteLabel, splitLabel, proceedLabel;
+    
     public ConcurrentEditingPrototype(Stage primaryStage) {
         this.primaryStage = primaryStage;
         setupUI();
     }
-
     
     private void setupUI() {
         stage = new Stage();
         stage.setTitle("Tu 44 Effort Log Editor");
         stage.setResizable(false);
-        stage.setWidth(900);  // adjusted width to match the picture
+        stage.setWidth(900);
         stage.setHeight(650);
 
         // Styles
-        String comboBoxStyle = "-fx-background-color: #FFFFFF; -fx-border-color: #AAAAAA; -fx-border-width: 1; -fx-font-size: 14pt;";  // adjusted font size
+        String comboBoxStyle = "-fx-background-color: #FFFFFF; -fx-border-color: #AAAAAA; -fx-border-width: 1; -fx-font-size: 14pt;";
         String buttonStyle = "-fx-background-color: #4E90A4; -fx-text-fill: #FFFFFF; -fx-border-radius: 5px; -fx-background-radius: 5px; -fx-font-size: 14pt;";
         String labelStyleBold = "-fx-font-family: 'Arial'; -fx-font-weight: BOLD; -fx-font-size: 14pt;";
-        String layoutBackgroundStyle = "-fx-background-color: #E0E0E0;";  // light gray background to match the picture
+        String layoutBackgroundStyle = "-fx-background-color: #E0E0E0;";
 
         VBox layout = new VBox(20);
         layout.setPadding(new Insets(20, 20, 20, 20));
         layout.setStyle(layoutBackgroundStyle);
 
         HBox combinedBox = new HBox(50);
-        combinedBox.setAlignment(Pos.CENTER_RIGHT);
+        combinedBox.setAlignment(Pos.CENTER);
 
         VBox selectProjectBox = new VBox(5);
         Label selectProjectLabel = new Label("1. Select the project to edit effort log");
@@ -53,7 +57,7 @@ public class ConcurrentEditingPrototype {
         projectSelect.setPrefWidth(300);
         selectProjectBox.getChildren().addAll(selectProjectLabel, projectSelect);
         selectProjectBox.setAlignment(Pos.CENTER_LEFT);
-        HBox.setHgrow(selectProjectBox, Priority.ALWAYS);
+        HBox.setHgrow(selectProjectBox, Priority.ALWAYS);  // Make sure this VBox grows as necessary
         combinedBox.getChildren().add(selectProjectBox);
 
         VBox clearLogBox = new VBox(5);
@@ -62,22 +66,22 @@ public class ConcurrentEditingPrototype {
         Button clearLogButton = new Button("Clear This Effort Log");
         clearLogButton.setStyle(buttonStyle);
         clearLogBox.getChildren().addAll(clearLogLabel, clearLogButton);
-        clearLogBox.setAlignment(Pos.CENTER_LEFT);
+        clearLogBox.setAlignment(Pos.CENTER_RIGHT);  // Added this line to align the VBox to the right within the HBox
         combinedBox.getChildren().add(clearLogBox);
-        combinedBox.setAlignment(Pos.CENTER_RIGHT);
+
         layout.getChildren().add(combinedBox);
 
-        Label effortLogEntryLabel = new Label("2.b. Select the Effort Log Entry to modify and make it the current selection:");
+        effortLogEntryLabel = new Label("2.b. Select the Effort Log Entry to modify and make it the current selection:");
         effortLogEntryLabel.setStyle(labelStyleBold);
-        ComboBox<String> logEntrySelect = new ComboBox<>(FXCollections.observableArrayList("User Upload"));
+        logEntrySelect = new ComboBox<>(FXCollections.observableArrayList("User Upload"));
         logEntrySelect.setStyle(comboBoxStyle);
         layout.getChildren().addAll(effortLogEntryLabel, logEntrySelect);
 
         VBox modifyLogBox = new VBox(5);
-        Label modifyLabel = new Label("3.a. Modify the Current Effort Log's attribute and \"Update this Entry\" when done:");
+        modifyLabel = new Label("3.a. Modify the Current Effort Log's attribute and \"Update this Entry\" when done:");
         modifyLabel.setStyle(labelStyleBold);
-        Label detailsLabel = new Label("Date: <date>    Start Time: <time>    Stop time: <time>");
-        detailsLabel.setFont(Font.font("Arial", 12));  // assuming a slightly smaller font for detail
+        detailsLabel = new Label("Date: <date>    Start Time: <time>    Stop time: <time>");
+        detailsLabel.setFont(Font.font("Arial", 12));
         modifyLogBox.getChildren().addAll(modifyLabel, detailsLabel);
         layout.getChildren().add(modifyLogBox);
 
@@ -87,35 +91,35 @@ public class ConcurrentEditingPrototype {
         layout.getChildren().add(grid);
 
         HBox updateBox = new HBox(15);
-        Label updateLabel = new Label("These attributes have been saved");
+        updateLabel = new Label("These attributes have been saved");
         updateLabel.setStyle(labelStyleBold);
-        Button updateButton = new Button("Update This Entry");
+        updateButton = new Button("Update This Entry");
         updateButton.setStyle(buttonStyle);
         updateBox.getChildren().addAll(updateLabel, updateButton);
         layout.getChildren().add(updateBox);
 
         VBox bottomActions = new VBox(10);
         VBox deleteActions = new VBox(10);
-        Label deleteLabel = new Label("3. b. Delete Current Entry:");
+        deleteLabel = new Label("3. b. Delete Current Entry:");
         deleteLabel.setStyle(labelStyleBold);
-        Button deleteButton = new Button("Delete This Entry");
+        deleteButton = new Button("Delete This Entry");
         deleteButton.setStyle(buttonStyle);
         deleteActions.getChildren().addAll(deleteLabel, deleteButton);
         bottomActions.getChildren().add(deleteActions);
 
         HBox nextLineActions = new HBox(30);
         VBox splitActions = new VBox(10);
-        Label splitLabel = new Label("3. c. Split Current Entry into two Entries:");
+        splitLabel = new Label("3. c. Split Current Entry into two Entries:");
         splitLabel.setStyle(labelStyleBold);
-        Button splitButton = new Button("Split Entry Into Two Entries");
+        splitButton = new Button("Split Entry Into Two Entries");
         splitButton.setStyle(buttonStyle);
         splitActions.getChildren().addAll(splitLabel, splitButton);
         nextLineActions.getChildren().add(splitActions);
 
         VBox proceedActions = new VBox(10);
-        Label proceedLabel = new Label("4. Proceed to Effort Log Console");
+        proceedLabel = new Label("4. Proceed to Effort Log Console");
         proceedLabel.setStyle(labelStyleBold);
-        Button proceedButton = new Button("Proceed to Effort Log Console");
+        proceedButton = new Button("Proceed to Effort Log Console");
         proceedButton.setStyle(buttonStyle);
         proceedActions.getChildren().addAll(proceedLabel, proceedButton);
         nextLineActions.getChildren().add(proceedActions);
@@ -125,7 +129,6 @@ public class ConcurrentEditingPrototype {
 
         Scene scene = new Scene(layout, 900, 650);
         stage.setScene(scene);
-
     }
 
     public void showWindow() {
