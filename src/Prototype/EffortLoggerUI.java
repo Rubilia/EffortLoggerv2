3aaa1fe8151ead57c5459d4f395b8b03ee536efb
rubilia <rubilia@DESKTOP-QVCNEN2>
2023-10-24 23:58:55 -0700

    Aded a scrollbar/author to main UI Updated readme.md

--- src/Prototype/EffortLoggerUI.java
+++ src/Prototype/EffortLoggerUI.java
@@ -6,17 +6,19 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
+import javafx.application.Platform;
 
 public class EffortLoggerUI extends Application {
 
-	public VBox createPrototypeBox(String prototypeName, String descriptionText) {
-	    // Prototype Name
+    public VBox createPrototypeBox(String prototypeName, String descriptionText, String authorName) {
+    	// Prototype Name
 	    Label title = new Label(prototypeName);
 	    title.setFont(Font.font("Arial", FontWeight.BOLD, 16));
 	    title.setStyle("-fx-text-fill: #333;");
@@ -32,6 +34,12 @@ public class EffortLoggerUI extends Application {
 	    
 	    TextFlow descriptionFlow = new TextFlow(descriptionLabel, description);
 
+        // Author
+        Label authorLabel = new Label("Author: " + authorName);
+        authorLabel.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+        authorLabel.setStyle("-fx-text-fill: #555;");
+        
+
 	    // Create Launch Button
 	    Button launchButton = new Button("Launch");
 	    launchButton.setPrefWidth(100);
@@ -45,8 +53,8 @@ public class EffortLoggerUI extends Application {
 	    launchButton.setOnMouseReleased(e -> launchButton.setStyle(buttonBaseStyle));
 
 	    // VBox for holding each prototype box's content
-	    VBox prototypeBoxContent = new VBox(10, title, descriptionFlow, launchButton);
-	    prototypeBoxContent.setAlignment(Pos.CENTER);
+        VBox prototypeBoxContent = new VBox(10, title, descriptionFlow, authorLabel, launchButton);
+        prototypeBoxContent.setAlignment(Pos.CENTER);
 	    prototypeBoxContent.setPadding(new Insets(10));
 	    prototypeBoxContent.setStyle("-fx-border-color: #ccc; -fx-border-width: 1px; -fx-border-radius: 15px; -fx-background-color: linear-gradient(#ffffff, #f3f3f3); -fx-background-radius: 15px; -fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.1), 5, 0, 0, 5);");
 	    return prototypeBoxContent;
@@ -65,24 +73,38 @@ public class EffortLoggerUI extends Application {
 
         VBox riskReductionContainer = new VBox(15);
         riskReductionContainer.setPadding(new Insets(10));
-
-        // Create the prototype boxes
-        VBox prototypeBox1 = createPrototypeBox("Initial Prototype", "Description for Initial Prototype");
-        VBox prototypeBox2 = createPrototypeBox("Prototype 2", "Description for Prototype 2");
-
-        riskReductionContainer.getChildren().addAll(prototypeBox1, prototypeBox2);
+        
+        // Edit these lines to specify information about your prototypes
+        riskReductionContainer.getChildren().addAll(
+                createPrototypeBox("Concurrent Editing Prototype", "Concurrent editing allows multiple users to work simultaneously. EffortLogger ensures changes are accurate and avoids overwrites.", "Ilia Rubashkin"),
+                createPrototypeBox("Prototype 2", "Description for Prototype 2", "Author2"),
+                createPrototypeBox("Prototype 3", "Description for Prototype 3", "Author3"),
+                createPrototypeBox("Prototype 4", "Description for Prototype 4", "Author4"),
+                createPrototypeBox("Prototype 5", "Description for Prototype 5", "Author5")
+        );
 
         mainContainer.getChildren().addAll(riskReductionTitle, riskReductionContainer);
 
-        Scene scene = new Scene(mainContainer, 350, 400); // Adjusted the height
+        ScrollPane scrollPane = new ScrollPane(mainContainer);
+        scrollPane.setFitToWidth(true); // to ensure the content adjusts to the width of the ScrollPane
+        scrollPane.setStyle("-fx-background-color: transparent; " +
+                "-fx-border-color: transparent; ");
+
+        Scene scene = new Scene(scrollPane, 450, 400);  // width increased to 450
         stage.setScene(scene);
 
-        stage.setMinWidth(375);  // Set minimum width
-        stage.setMinHeight(425);  // Adjusted minimum height
-        stage.show();
-    }
+        stage.setMinWidth(500);  // Set the minimum width to 500
+        stage.setMinHeight(650);  // Set the minimum height
 
-    public static void main(String[] args) {
-        launch(args);
+        stage.show();
+
+        // Use Platform.runLater to defer the lookup styling
+        Platform.runLater(() -> {
+            scrollPane.lookup(".scroll-bar").setStyle("-fx-background-color: transparent;");
+            scrollPane.lookup(".scroll-bar .thumb").setStyle("-fx-background-color: rgba(0, 123, 255, 0.5); -fx-background-radius: 2;");
+        });
     }
-}
+        public static void main(String[] args) {
+            launch(args);
+        }
+    }
